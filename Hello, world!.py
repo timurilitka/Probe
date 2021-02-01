a8dc03860348d0fe79823624423c8d7b0a7ecb5d
timurilitka <salikov_timur@mail.ru>
2021-02-02 01:38:11 +0300

    And more fuck times

--- Hello, world!.py
+++ Hello, world!.py
@@ -1,5 +1,6 @@
 from random import randint
 
 number = randint(0,100)
+second_number = randint(0,100)
 
-print(f'Fuck you {number} times!')
+print(f'Fuck you {number} times and {second_number} times more!')
